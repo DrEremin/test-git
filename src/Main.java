7f20b9d06061d6a91754e04c8e119206eaf59cc7
DrEremin <ivaneremin1979@gmail.com>
2022-01-25 14:03:23 +0300

    Add the field job to class models.Person class
    Add job to constructor of object person to main()

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import models.Person;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test-git");
-        Person person = new Person("Иван", "Иванов", 25);
+        Person person = new Person("Иван", "Иванов", "data engineer", 25);
         System.out.println(person);
     }
 }
--- src/models/Person.java
+++ src/models/Person.java
@@ -4,13 +4,16 @@ public class Person {
 
     private String firstName;
     private String lastName;
+    private String job;
     private int age;
 
     public Person(String firstName,
                   String lastName,
+                  String job,
                   int age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.job = job;
         this.age = age;
     }
 
@@ -20,6 +23,10 @@ public class Person {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
     }
 
     public void setFAge(int age) {
@@ -32,6 +39,10 @@ public class Person {
 
     public String getLastName() {
         return this.lastName;
+    }
+
+    public String getJob() {
+        return this.job;
     }
 
     public int getFAge() {
